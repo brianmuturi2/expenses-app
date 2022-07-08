304d455081d9c2e7c4dc248eafefd3dc9c1c4b05
brianmuturi2 <brianmuturi2@gmail.com>
2022-07-08 09:30:08 +0300

    feat(expenses): added two way binding to expense form

--- src/components/Expenses/NewExpense/ExpenseForm.js
+++ src/components/Expenses/NewExpense/ExpenseForm.js
@@ -25,6 +25,7 @@ function ExpenseForm() {
     function submitHandler(event) {
         event.preventDefault();
         console.log('user input is ', userInput);
+        setUserInput(prevState => ({...prevState, title: '', amount: '', date: ''}));
     }
 
     return (
@@ -32,15 +33,15 @@ function ExpenseForm() {
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler}/>
+                    <input type="text" onChange={titleChangeHandler} value={userInput.title}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} value={userInput.amount}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2022-12-01" onChange={dateChangeHandler}/>
+                    <input type="date" min="2019-01-01" max="2022-12-01" onChange={dateChangeHandler} value={userInput.date}/>
                 </div>
             </div>
             <div className="new-expense__actions">
